7b5b1ed248be96610df967edb259b9b3d1704462
Nguyễn Mạnh Cường <nguyenmanhcuong03@vccorp.vn>
2020-07-02 00:32:05 +0700

    Fake large items

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,31 +1,34 @@
+import { loremIpsum } from 'lorem-ipsum';
+
+const tasks = Array.from({ length: 300 }, (_, index) => ({
+  id: `task-${index}`,
+  content: loremIpsum(),
+}));
+
+const taskIds = tasks.map(({ id }) => id);
+
+const convertArrayToObject = (array, key, prefix = '') => array.reduce((object, item) => ({
+  ...object,
+  [`${prefix}${item[key]}`]: item,
+}), {});
+
 const initialData = {
-  tasks: {
-    'task-1': { id: 'task-1', content: 'Take out me away 1' },
-    'task-2': { id: 'task-2', content: 'Take out me away 2' },
-    'task-3': { id: 'task-3', content: 'Take out me away 3' },
-    'task-4': { id: 'task-4', content: 'Take out me away 4' },
-    'task-5': { id: 'task-5', content: 'Take out me away 5' },
-    'task-6': { id: 'task-6', content: 'Take out me away 6' },
-    'task-7': { id: 'task-7', content: 'Take out me away 7' },
-    'task-8': { id: 'task-8', content: 'Take out me away 8' },
-    'task-9': { id: 'task-9', content: 'Take out me away 9' },
-    'task-10': { id: 'task-10', content: 'Take out me away 10' },
-  },
+  tasks: convertArrayToObject(tasks, 'id'),
   columns: {
     'column-1': {
       id: 'column-1',
       title: 'To do',
-      taskIds: ['task-1', 'task-2', 'task-3', 'task-4', 'task-5'],
+      taskIds: taskIds.slice(0, 99),
     },
     'column-2': {
       id: 'column-2',
       title: 'Inprogress',
-      taskIds: ['task-6', 'task-7', 'task-8', 'task-9', 'task-10'],
+      taskIds: taskIds.slice(100, 199),
     },
     'column-3': {
       id: 'column-3',
       title: 'Done',
-      taskIds: [],
+      taskIds: taskIds.slice(200, 299),
     },
   },
   columnOrder: ['column-1', 'column-2', 'column-3'],
